39eeffdb221d1a8e012b743ee41e997660bd5a05
Bryan W. Lewis <blewis@illposed.net>
2022-04-29 18:35:47 -0400

    initial commit (in process)

--- R/redis.R
+++ R/redis.R
@@ -1,11 +1,23 @@
 #' Redis-based futures
 #'
-#' Use the Redis key/value database to define partially fault-tolerant task
-#' queues for elastic distributed computing.
+#' Use the Redis key/value database to define partially fault-tolerant,
+#' asynchronous task queues for elastic distributed computing.
 #'
 #' @inheritParams RedisFuture
 #' @importFrom redux redis_config
 #' @return An object of class [RedisFuture].
+#' @example
+#' plan(redis)
+#' data(warpbreaks)
+#' trials <- 1000
+#' Map(\(i) future({
+#'     i <- sample(NROW(warpbreaks), replace=TRUE)
+#'     coefficients(lm(breaks ~ wool * tension, data = warpbreaks[i,]))
+#'   }, lazy = TRUE, seed = TRUE), seq(trials)
+#" ) |> (
+#'        \(v) Reduce(function(x,y) rbind(x, value(y)), init=NULL, v)
+#'      )() -> ans
+#' plot(density(ans[, 2]), main = "wool coef")
 #' @export
 redis <- function(...,
                   queue = "RJOBS",
