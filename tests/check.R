a159780f8888a21665f6779cadd5e3423a2811ed
Bryan W. Lewis <blewis@illposed.net>
2022-05-14 18:00:29 -0400

    Increased default check timeout to pass lass resolved test

--- tests/check.R
+++ tests/check.R
@@ -2,6 +2,7 @@ library("future.redis")
 library("future.tests")
 
 plan(redis)
+removeQ()
 startLocalWorkers(1, linger=1, log="/dev/null")
-check("redis", timeout=5)
+check("redis", timeout=10)
 removeQ()
