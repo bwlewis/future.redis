a00fa2b61ae8669686faa738e47ce8b3664ab150
Bryan W. Lewis <blewis@illposed.net>
2022-05-14 18:43:29 -0400

    Fixed issue with test cleanup (check function exit_value = FALSE to avoid premature quit)

--- tests/check.R
+++ tests/check.R
@@ -1,13 +1,8 @@
 library("future.redis")
 library("future.tests")
 
-test <- function()
-{
-  on.exit({removeQ()})
-  plan(redis)
-  removeQ()
-  startLocalWorkers(1, linger=1)
-  check("redis", timeout=10)
-}
-
-test()
+plan(redis)
+removeQ()
+startLocalWorkers(1, linger=1)
+check("redis", timeout = 10, exit_value = FALSE)
+removeQ()
